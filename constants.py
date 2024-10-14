324a73b48bda478f3b0c4f7820184d3e059b8fe7
Csilla Bukki <csillabukki@csillas-air.lan>
2024-10-14 13:05:48 -0700

    API info

---
+++ constants.py
@@ -0,0 +1,16 @@
+
+#YouTube Data API v3
+API_KEY_YT1 = "AIzaSyBwQs8zpR7vmFMWaTgMxk5BYuledF21SkQ"
+API_KEY_YT2 = "AIzaSyCS3pe8qx0YMeKLkAB1ahpXyakyPNbbHdQ"
+API_KEY_YT3 = "AIzaSyAOeQAbxJ3hThIKBZKaVxp4lpsGCz3bh40"
+BASE_URL_YT = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="
+
+
+#Ninjas API Exercise
+API_KEY_NINJAS =  "GGnSyuKegKiVdbrYNP0K8g==831nq3Bvb0udGPIB"
+BASE_URL_EXERCISE = "https://api.api-ninjas.com/v1/exercises?muscle="
+
+
+print("constants.py")
+
+#BASE_URL_YT = f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=Elbow plank&key={API_KEY_YT}"
